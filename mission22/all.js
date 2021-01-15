31ce538361bced15b749d916214e7bd076ba75b1
PKTseng <jk0910948659@Gmail.com>
2021-01-15 20:43:32 +0800

    Add container addEventListener function

---
+++ mission22/all.js
@@ -0,0 +1,18 @@
+const container = document.querySelector('.container')
+const seats = document.querySelectorAll('.row .seat:not(.occupied)')
+const count = document.querySelector('#count')
+const total = document.querySelector('#total')
+const movieSelect = document.querySelector('#movie')
+
+
+
+// 監聽容器內座位數值的變化
+container.addEventListener('click', e=>{
+	// contains 會返回一個 boolean 值
+	if(e.target.classList.contains('seat') && !e.target.classList.contains('occupied')){
+		e.target.classList.toggle('selected')
+		updateSelectCount()
+	}
+})
+
+updateSelectCount()
