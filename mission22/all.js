593c15f499bd06986f7b8027862a93d069b9a220
PKTseng <jk0910948659@Gmail.com>
2021-01-15 23:18:02 +0800

    Catch dom

---
+++ mission22/all.js
@@ -0,0 +1,6 @@
+const container = document.querySelector('.container')
+const seats = document.querySelectorAll('.row .seat:not(.occupied)')
+const count = document.querySelector('#count')
+const total = document.querySelector('#total')
+const movieSelect = document.querySelector('#movie')
+
