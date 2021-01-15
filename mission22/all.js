a5de21d8d655c481df05de1fa16da932b6017922
PKTseng <jk0910948659@Gmail.com>
2021-01-15 23:18:59 +0800

    Add container function check seats change

--- mission22/all.js
+++ mission22/all.js
@@ -4,3 +4,12 @@ const count = document.querySelector('#count')
 const total = document.querySelector('#total')
 const movieSelect = document.querySelector('#movie')
 
+
+
+// 監聽容器內座位數值的變化
+container.addEventListener('click', e=>{
+	// contains 會返回一個 boolean 值
+	if(e.target.classList.contains('seat') && !e.target.classList.contains('occupied')){
+		e.target.classList.toggle('selected')
+	}
+})
