fd8b4070c84b82010e878879cdc2355def9b5c84
PKTseng <jk0910948659@Gmail.com>
2021-01-15 23:21:55 +0800

    Select seats and calculation price & quantity price

--- mission22/all.js
+++ mission22/all.js
@@ -3,6 +3,7 @@ const seats = document.querySelectorAll('.row .seat:not(.occupied)')
 const count = document.querySelector('#count')
 const total = document.querySelector('#total')
 const movieSelect = document.querySelector('#movie')
+let ticketPrice = +movieSelect.value
 
 // 計算選擇的座位數量價格
 function updateSelectCount(){
@@ -17,6 +18,19 @@ function updateSelectCount(){
 	const seatsIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat))
 	localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex))
 }
+
+// 依照選擇的電影變更價格
+movieSelect.addEventListener('change', e=>{
+	ticketPrice = +e.target.value
+
+	// 查看我們選擇的電影索引跟價格
+	// console.log(e.target.selectedIndex, e.target.value) 
+
+	// 將電影索引跟價格的參數塞到 setMovieData 裡面
+	setMovieData(e.target.selectedIndex, e.target.value)
+	updateSelectCount()//執行
+})
+
 
 // 監聽容器內座位數值的變化
 container.addEventListener('click', e=>{
