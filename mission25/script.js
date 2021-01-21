bacc9d7876ff3dc3f1af17de499f9fe2eefabf8a
PKTseng <jk0910948659@Gmail.com>
2021-01-21 22:40:33 +0800

    Catch dom & update songs details

---
+++ mission25/script.js
@@ -0,0 +1,21 @@
+const musicContainer = document.querySelector('#music-container')
+const progressContainer = document.querySelector('#progress-container')
+const progress = document.querySelector('#progress')
+const audio = document.querySelector('#audio')
+const cover = document.querySelector('#cover')
+const prev = document.querySelector('#prev')
+const play = document.querySelector('#play')
+const next = document.querySelector('#next')
+const title = document.querySelector('#title')
+
+const songs = ['hey', 'summer', 'ukulele']
+
+let songsIndex = 2
+
+loadSong(songs[songsIndex])
+
+function loadSong(song) {
+	title.innerText = song
+	audio.src= `music/${song}.mp3` 
+	cover.src= `image/${song}.jpg` 
+}
