4135209aea139b1d05b5dfbbc9b488a283e1b1b0
PKTseng <jk0910948659@Gmail.com>
2021-01-22 13:40:33 +0800

    Add progressBar

--- mission25/script.js
+++ mission25/script.js
@@ -56,6 +56,20 @@ function nextSong() {
   playSong();
 }
 
+function updateProgress(e) {
+  const { duration, currentTime } = e.srcElement;
+  const progressPercent = (currentTime / duration) * 100;
+  progress.style.width = `${progressPercent}%`;
+}
+
+// function setProgress(e) {
+// 	const width = this.clientWidth
+// 	const clickX = e.offsetX
+// 	const duration = audio.duration
+
+// 	audio.currentTime = (clickX / width) * duration
+// }
+
 playBtn.addEventListener("click", () => {
   const isPlaying = musicContainer.classList.contains("play");
   if (isPlaying) {
@@ -68,3 +82,7 @@ playBtn.addEventListener("click", () => {
 // change song
 PrevBtn.addEventListener("click", preSong);
 nextBtn.addEventListener("click", nextSong);
+
+audio.addEventListener("timeupdate", updateProgress);
+
+// progressContainer.addEventListener("click", setProgress);
