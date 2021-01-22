39cef7b824753ac51d7ade959a067c90fae3f53f
PKTseng <jk0910948659@Gmail.com>
2021-01-22 13:11:26 +0800

    Add change song

--- mission25/script.js
+++ mission25/script.js
@@ -1,65 +1,70 @@
-const musicContainer = document.querySelector('#music-container')
-const progressContainer = document.querySelector('#progress-container')
-const progress = document.querySelector('#progress')
-const audio = document.querySelector('#audio')
-const cover = document.querySelector('#cover')
-const PrevBtn = document.querySelector('#prev')
-const playBtn = document.querySelector('#play')
-const nextBtn = document.querySelector('#next')
-const title = document.querySelector('#title')
+const musicContainer = document.querySelector("#music-container");
+const progressContainer = document.querySelector("#progress-container");
+const progress = document.querySelector("#progress");
+const audio = document.querySelector("#audio");
+const cover = document.querySelector("#cover");
+const PrevBtn = document.querySelector("#prev");
+const playBtn = document.querySelector("#play");
+const nextBtn = document.querySelector("#next");
+const title = document.querySelector("#title");
 
-const songs = ['hey', 'summer', 'ukulele']
+const songs = ["hey", "summer", "ukulele"];
 
-let songsIndex = 2
+let songsIndex = 2;
 
-loadSong(songs[songsIndex])
+loadSong(songs[songsIndex]);
 
 function loadSong(song) {
-	title.innerText = song
-	audio.src= `music/${song}.mp3` 
-	cover.src= `images/${song}.jpg` 
+  title.innerText = song;
+  audio.src = `music/${song}.mp3`;
+  cover.src = `images/${song}.jpg`;
 }
 
-
 function playSong() {
-	musicContainer.classList.add('play')
-	playBtn.querySelector('i.fas').classList.remove('fa-play')
-	playBtn.querySelector('i.fas').classList.add('fa-pause')
-  
-	audio.play()
+  musicContainer.classList.add("play");
+  playBtn.querySelector("i.fas").classList.remove("fa-play");
+  playBtn.querySelector("i.fas").classList.add("fa-pause");
+
+  audio.play();
 }
 
 function pauseSong() {
-	musicContainer.classList.remove('play')
-	playBtn.querySelector('i.fas').classList.add('fa-play')
-	playBtn.querySelector('i.fas').classList.remove('fa-pause')
-  
-	audio.pause()
+  musicContainer.classList.remove("play");
+  playBtn.querySelector("i.fas").classList.add("fa-play");
+  playBtn.querySelector("i.fas").classList.remove("fa-pause");
+
+  audio.pause();
 }
 
-function preSong(){
-	songsIndex--
-  
-	if (songsIndex < 0) {
-		songsIndex = songs.length - 1
-	}
-  
-	playSong()
+function preSong() {
+  songsIndex--;
+
+  if (songsIndex < 0) {
+    songsIndex = songs.length - 1;
+  }
+
+  playSong();
 }
 
+function nextSong() {
+  songsIndex++;
 
+  if (songIndex > songs.length - 1) {
+    songIndex = 0;
+  }
 
-playBtn.addEventListener('click', () =>{
-	const isPlaying = musicContainer.classList.contains('play')
-	if (isPlaying) {
-		pauseSong()
-	}else{
-		playSong()
-	}
-})
+  playSong();
+}
 
-
+playBtn.addEventListener("click", () => {
+  const isPlaying = musicContainer.classList.contains("play");
+  if (isPlaying) {
+    pauseSong();
+  } else {
+    playSong();
+  }
+});
 
 // change song
-PrevBtn.addEventListener('click', preSong)
-nextBtn.addEventListener('click', nextSong)
+PrevBtn.addEventListener("click", preSong);
+nextBtn.addEventListener("click", nextSong);
