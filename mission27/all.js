0e56239b213dfd8eb472becda06d263a46bae56e
PKTseng <jk0910948659@Gmail.com>
2021-01-28 17:26:45 +0800

    Add annotation

--- mission27/all.js
+++ mission27/all.js
@@ -35,30 +35,26 @@ const words = [
   "loving",
 ];
 
-function showRandomWord() {
-  const showText = getRandomWord();
-  word.innerHTML = getRandomWord();
-}
-showRandomWord();
-// console.log(getRandomWord());
-
-// 抓取裡面的值
+// 抓取陣列內的值
 function getRandomWord() {
   return words[Math.floor(Math.random() * words.length)];
 }
 
+// 把值綁定到 DOM 上
 function showRandomWord() {
   showWord = getRandomWord();
   word.innerHTML = showWord;
 }
 
-showRandomWord();
-// console.log(getRandomWord());
+showRandomWord(); // 執行
+
+// 答對加分，同時把分數綁到 DOM 上
 function updateScore() {
   score++;
   scoreEl.innerHTML = score;
 }
 
+// 在輸入框輸入文字同時跟顯示的文字做對比
 text.addEventListener("input", (e) => {
   let inputText = e.target.value;
   if (inputText === showWord) {
