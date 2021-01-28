05802e883c948b661ef375d50fc05dd9081271ac
PKTseng <jk0910948659@Gmail.com>
2021-01-28 17:14:03 +0800

    Get word and innerHTML DOM

---
+++ mission27/all.js
@@ -0,0 +1,56 @@
+const word = document.getElementById("word");
+const text = document.getElementById("text");
+const scoreEl = document.getElementById("score");
+const timeEl = document.getElementById("time");
+const endgameEl = document.getElementById("end-game-container");
+const settingsBtn = document.getElementById("settings-btn");
+const settings = document.getElementById("settings");
+const settingsForm = document.getElementById("settings-form");
+const difficultySelect = document.getElementById("difficulty");
+
+let randomWord = 0;
+let score = 0;
+let time = 10;
+
+const words = [
+  "sigh",
+  "tense",
+  "airplane",
+  "ball",
+  "pies",
+  "juice",
+  "warlike",
+  "bad",
+  "north",
+  "dependent",
+  "steer",
+  "silver",
+  "highfalutin",
+  "superficial",
+  "quince",
+  "eight",
+  "feeble",
+  "admit",
+  "drag",
+  "loving",
+];
+
+function showRandomWord() {
+  const showText = getRandomWord();
+  word.innerHTML = getRandomWord();
+}
+showRandomWord();
+// console.log(getRandomWord());
+
+// 抓取裡面的值
+function getRandomWord() {
+  return words[Math.floor(Math.random() * words.length)];
+}
+
+function showRandomWord() {
+  showWord = getRandomWord();
+  word.innerHTML = showWord;
+}
+
+showRandomWord();
+// console.log(getRandomWord());
