4702d7e82d03880cecd965a6ebcfa09b6719bd84
PKTseng <jk0910948659@Gmail.com>
2021-02-01 14:33:12 +0800

    Check speak number

--- mission28/all.js
+++ mission28/all.js
@@ -14,6 +14,29 @@ window.SpeechRecognition =
 
 let recognition = new window.SpeechRecognition()
 
+function checkMsg(msg) {
+  const num = +msg
+  if (Number.isNaN(num)) {
+    msgEl.innerHTML = `<div>請說數字 !</div>`
+  }
+
+  if (num > 100 || num < 1) {
+    msgEl.innerHTML = `<div>數字只能介於 1~100 之間</div>`
+  }
+
+  if (num === randomNumber) {
+    msgEl.innerHTML = `
+      <h2>恭喜猜中r! <br><br>
+      就是 ${num}</h2>
+      <button class="play-again" id="play-again">再玩一次</button>
+      `
+  } else if (num > randomNumber) {
+    msgEl.innerHTML = `<div>再低</div>`
+  } else {
+    msgEl.innerHTML = `<div>再高</div>`
+  }
+}
+
 recognition.start()
 recognition.addEventListener('result', (e) => {
   // 語音測試一下
@@ -26,6 +49,7 @@ recognition.addEventListener('result', (e) => {
   <div>You said:</div>
   <span class="box">${msg}</span>
   `
+  checkMsg(msg)
 })
 
 recognition.addEventListener('end', () => recognition.start())
