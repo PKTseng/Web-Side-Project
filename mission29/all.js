d713f361b5a4b6c7a5cf96dbe564e730d03cfd32
PKTseng <jk0910948659@Gmail.com>
2021-02-03 16:42:41 +0800

    Catch dom

---
+++ mission29/all.js
@@ -0,0 +1,4 @@
+const dayEl = document.querySelector('#day')
+const hourEl = document.querySelector('#hours')
+const minutesEL = document.querySelector('#minutes')
+const secondsEl = document.querySelector('#seconds')
