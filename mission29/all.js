ed7256f2803bc228e57f0da0762175a177181333
PKTseng <jk0910948659@Gmail.com>
2021-02-03 16:43:25 +0800

    Get year time

--- mission29/all.js
+++ mission29/all.js
@@ -2,3 +2,15 @@ const dayEl = document.querySelector('#day')
 const hourEl = document.querySelector('#hours')
 const minutesEL = document.querySelector('#minutes')
 const secondsEl = document.querySelector('#seconds')
+// const times1 = new Date()
+// console.log(times1)
+// console.log(typeof times1)
+
+const nowYear = new Date().getFullYear()
+// getFullYear() 方法用來取得一個 Date 物件的年份，時區是本地時間 (local time)
+// getFullYear() 會把物件型別轉成數字
+// console.log(nowYear)
+// console.log(typeof nowYear)
+
+const newYear = new Date(`January 01 ${nowYear + 1} 00:00:00`)
+// console.log(newYearTime)
