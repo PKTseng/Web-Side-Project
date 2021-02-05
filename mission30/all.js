d8671ba1755eef171d59dcaa908f1a7915ff9598
PKTseng <jk0910948659@Gmail.com>
2021-02-05 17:12:03 +0800

    Refactor updateSavedColumns function

--- mission30/all.js
+++ mission30/all.js
@@ -13,6 +13,7 @@ let backLogListArray = []
 let progressListArray = []
 let completeListArray = []
 let onHoldListArray = []
+let ListArrays = []
 
 function getSaveColumns() {
   if (localStorage.getItem('backLogItems')) {
@@ -21,19 +22,34 @@ function getSaveColumns() {
     completeListArray = JSON.parse(localStorage.completeListItems)
     onHoldListArray = JSON.parse(localStorage.onHoldListItems)
   } else {
-    // 設定初始值
+    // 設定初始值，不然每個欄位都是空的
     backLogListArray = ['Release the course', 'Sit back and relax']
     progressListArray = ['Work on projects', 'Listen to music']
     completeListArray = ['Being cool', 'Getting stuff done']
     onHoldListArray = ['Being uncool']
   }
 }
-getSaveColumns()
 
 //  設定 localStorage 的 key 跟　value
 function updateSavedColumns() {
-  localStorage.setItem('backLogItems', JSON.stringify(backLogListArray))
-  localStorage.setItem('progressListArray', JSON.stringify(progressListArray))
-  localStorage.setItem('completeListArray', JSON.stringify(completeListArray))
-  localStorage.setItem('onHoldListArray', JSON.stringify(onHoldListArray))
+  // localStorage.setItem('backLogItems', JSON.stringify(backLogListArray))
+  // localStorage.setItem('progressListItems', JSON.stringify(progressListArray))
+  // localStorage.setItem('completeListItems', JSON.stringify(completeListArray))
+  // localStorage.setItem('onHoldListItems', JSON.stringify(onHoldListArray))
+
+  // 將上面4行改寫成下面型式
+  let ListArrays = [
+    backLogListArray,
+    progressListArray,
+    completeListArray,
+    onHoldListArray,
+  ]
+
+  const arrayNames = ['backLog', 'progress', 'complete', 'onHold']
+  arrayNames.forEach((arrayName, index) => {
+    localStorage.setItem(`${arrayName}Items`, JSON.stringify(ListArrays[index]))
+  })
 }
+
+getSaveColumns()
+updateSavedColumns()
