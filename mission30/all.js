54227ca5f6ffdf078e955b2b45e9f985524c8ba7
PKTseng <jk0910948659@Gmail.com>
2021-02-06 18:53:55 +0800

    person name Inner dom

--- mission30/all.js
+++ mission30/all.js
@@ -14,19 +14,28 @@ const richestPeople = [
   'Larry Page',
 ]
 
+let dragStarIndex
+const listItems = []
+
 creatListItem()
 
 function creatListItem() {
-  ;[...richestPeople].forEach((person, index) => {
-    const listItem = document.createElement('li')
-    listItem.setAttribute('dragIndex', index)
+  ;[...richestPeople]
+    .map((a) => ({ value: a, sort: Math.random() }))
+    .sort((a, b) => a.sort - b.sort)
+    .map((a) => a.value)
+    .forEach((person, index) => {
+      const listItem = document.createElement('li')
+      listItem.setAttribute('dragIndex', index)
 
-    listItem.innerHTML = `
+      listItem.innerHTML = `
     <span class='number'>${index}+1</span>
     <div class='draggable' draggable='true'>
       <p class='person-name'>${person}</p>
       <i class='fas fa-grip-lines'></i>
     </div>
     `
-  })
+      listItems.push(listItem)
+      draggableList.append(listItem)
+    })
 }
