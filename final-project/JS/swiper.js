d4ad988143e0296c9b121a342cd91d5e07eae63c
PKTseng <jk0910948659@Gmail.com>
2021-03-09 20:59:40 +0800

    Add swiper autoplay settings

--- final-project/JS/swiper.js
+++ final-project/JS/swiper.js
@@ -1,16 +1,26 @@
-var galleryThumbs = new Swiper('.gallery-thumbs', {
+const galleryThumbs = new Swiper('.gallery-thumbs', {
   spaceBetween: 10,
   slidesPerView: 4,
   loop: true,
   freeMode: true,
-  loopedSlides: 5, //looped slides should be the same
+  loopedSlides: 10, //looped slides should be the same
   watchSlidesVisibility: true,
   watchSlidesProgress: true,
+  speed: 1000,
+  // autoplay: {
+  //   delay: 2000,
+  //   disableOnInteraction: true,
+  // },
 })
-var galleryTop = new Swiper('.gallery-top', {
+const galleryTop = new Swiper('.gallery-top', {
   spaceBetween: 10,
   loop: true,
-  loopedSlides: 5, //looped slides should be the same
+  loopedSlides: 10, //looped slides should be the same
+  speed: 1000,
+  // autoplay: {
+  //   delay: 2000,
+  //   disableOnInteraction: true,
+  // },
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
